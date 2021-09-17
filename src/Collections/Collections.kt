80c56f9e3da14cb0980724ce74002cb83b68f2fc
RouberR <Rouber1998@gmail.com>
2021-09-18 00:27:30 +0300

    test 0.6

--- src/Collections/Collections.kt
+++ src/Collections/Collections.kt
@@ -1,5 +1,8 @@
 package Collections
 //Коллекции List, set, mutableList, mutableSet
+
+data class Recipe(var name: String)
+
 fun main() {
     var mShoppingList = mutableListOf("Tea", "Eggs", "Milk")
     println("mShoppingList original: $mShoppingList")
@@ -25,6 +28,22 @@ fun main() {
     println("mShoppingSet items added: $mShoppingSet")
     mShoppingList = mShoppingSet.toMutableList()
     println("mShoppingList new version: $mShoppingList")
+    println("-----------------------------------------------------")
+
+
+    val r1 = Recipe("Chicken Soup")
+    val r2 = Recipe("Quinoa Salad")
+    val r3 = Recipe("Thai Curry")
+    val r4 = Recipe("Jambalaya")
+    val r5 = Recipe("Sausage Rolls")
+    val mRecipeMap = mutableMapOf("Recipe1" to r1, "Recipe2" to r2, "Recipe3" to r3)
+    println("mRecipeMap original: $mRecipeMap")
+    val recipesToAdd = mapOf("Recipe4" to r4, "Recipe5" to r5)
+    mRecipeMap.putAll(recipesToAdd)
+    println("mRecipeMap updated: $mRecipeMap")
+    if(mRecipeMap.containsKey("Recipe1")){
+        println("Recipe1 is: ${mRecipeMap.getValue("Recipe1")}")
+    }
 
 
 }
